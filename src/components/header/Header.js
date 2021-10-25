97c6a3787d51803be62e25c9059c700069834043
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-10-24 17:59:52 -0700

    creating login and logout in header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,17 @@ import './style.css'
 
 export default function Header() {
     return (
-        <div>
-            <h1>hello!</h1>
+        <div className='header'>
+            <div className='header-text'>
+                <h1>Hello!</h1>
+            </div>
+            <div className='signin-signout'>
+                <p>LogIn</p>
+                <i class="fa fa-sign-in" aria-hidden="true"></i>
+                <p>LogOut</p>
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+            </div>
+            
         </div>
     )
 }
